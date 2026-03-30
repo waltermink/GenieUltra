48352dd6261fb73993cd29c0bce1c21d8b9b630d
Walter Mink <wwmink@crimson.ua.edu>
2026-03-30 13:19:40 -0500

    Initial Commit

---
+++ GenieUltra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GenieUltra
+//
+//  Created by Walter Mink on 3/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GenieUltra/GenieUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenieUltraApp.swift
+//  GenieUltra
+//
+//  Created by Walter Mink on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GenieUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
